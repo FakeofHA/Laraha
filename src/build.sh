becc78ed42695aa5ffae134abacf794db28a39f8
Anh.Nguyen <hoanganh.19990210@outlook.com>
2025-07-07 20:58:50 +0700

    update

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,8 @@
-composer global require laravel/installer temporary
+composer create-project laravel/laravel temporary
 composer require laravel/ui
 php artisan ui bootstrap --auth
+npm install && npm run build
+php artisan migrate
 cp -r temporary/. .
 rm -r temporary
 rm -r build.sh
